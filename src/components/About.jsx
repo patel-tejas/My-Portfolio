01a826b62b5f8827928b89f090ac62495b9c3ac8
Tejas Patel <115111286+Techno-Tez@users.noreply.github.com>
2023-11-12 11:47:50 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,14 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
 
-            <p className='mt-5 text-base md:text-xl md:mt-20'>I started my coding journey in 2022. I studied Python as my first language and then shifted to Java. I have
+            <p className='mt-5 text-base md:text-xl md:mt-20'>I started my coding journey in 2022. I started with Python as my first language and then shifted to Java. I have
             a firm grip on both of them. Along with that I am currently working and enhancing myself in the area of web
-            development. I have worked with ReactJs and currently learning in-depth about MERN technology. I am also
+            development. I have worked with ReactJs and currently learning in-depth about MERN framework. I am also
             familiar with Data Structures and Algorithm and have solved 100+ questions on Leetcode.
             </p>
 
             <br/>
-            <p className='text-base md:text-xl'>I have created some of the projects like Weather Application, Cloud Notebook to store user's notes, News application etc. I have worked with APIs and also used backend through MongoDb to serve backend.</p>
+            <p className='text-base md:text-xl'>I have created some of the projects like Weather Application, Cloud Notebook to store user's notes, News application etc. I have created APIs on nodejs using MongoDb as database service. I have also worked on ExpressJs to make the backend services more efficient.</p>
         </div>
     </div>
   )
